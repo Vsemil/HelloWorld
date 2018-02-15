0219e7108d6038d2e1cccce1cd1fba9bf74c25c3
Vsemil <32932049+Vsemil@users.noreply.github.com>
2018-02-15 22:37:44 +0300

    Initial commit

---
+++ src/main/java/com/example/HelloWorld/Visit.java
@@ -0,0 +1,17 @@
+package com.example.HelloWorld;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+    @Entity
+    public class Visit {
+
+        @Id
+        @GeneratedValue
+        public Long id;
+
+        public String description;
+    }
+
